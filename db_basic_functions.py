d1888d04b71aabef67c0d5e3479656beb76f5c13
gjf7128 <gjf7128@rit.edu>
2023-11-04 12:33:12 -0400

    change_name_of_collection() implemented. needs testing via python

--- db_basic_functions.py
+++ db_basic_functions.py
@@ -3,6 +3,7 @@ import yaml
 import os
 from sshtunnel import SSHTunnelForwarder
 
+
 def execute_sql(sql, args={}):
     config = {}
     yml_path = os.path.join(os.path.dirname(__file__), 'db.yml')
@@ -16,52 +17,61 @@ def execute_sql(sql, args={}):
             server.start()
             print("SSH tunnel established")
             conn = psycopg2.connect(dbname=config['database'],
-                            user=config['user'],
-                            password=config['password'],
-                            host=config['host'],
-                            port=server.local_bind_port)
+                                    user=config['user'],
+                                    password=config['password'],
+                                    host=config['host'],
+                                    port=server.local_bind_port)
             cur = conn.cursor()
             cur.execute(sql, args)
             conn.commit()
             list_of_tuples = cur.fetchall()
             conn.close()
             return list_of_tuples
-        
+
     except Exception as e:
         print("Connection failed\n")
         print(e)
 
+
 def main():
     print(execute_sql("SELECT COUNT(*) FROM authors"))
 
+
 def addBookToCollection(collectionid, bookid):
-    #Users can add and delete books from their collection Requirement
-    #This function is just for ADDING
-    #This function still needs work
+    # Users can add and delete books from their collection Requirement
+    # This function is just for ADDING
+    # This function still needs work
     sqlStatement = 'INSERT INTO contains(collectionid, bookid) VALUES ({}, {})'.format(collectionid, bookid)
     return execute_sql(sqlStatement)
 
+
 def deleteBookFromCollection(collectionid, bookid):
-    #Users can add and delete books from their collection Requirement
-    #This function is just for DELETING
-    #This function still needs work
+    # Users can add and delete books from their collection Requirement
+    # This function is just for DELETING
+    # This function still needs work
     sqlStatement = 'DELETE FROM contains WHERE collectionid={} AND bookid={}'.format(collectionid, bookid)
     return execute_sql(sqlStatement)
 
-def changeNameOfCollection(userId):
-    #Users can modify the name of a collection. They can also delete an entire collection Requirement
-    #Use this function like this: cur.execute(deleteBookFromCollection(collectionid, bookid))
-    nameOfCollectionToDelete = input('Enter name of collection to be re-named')
-    nameOfNewCollection = input('Enter new name of collection')
 
-    #The following LOC depends on collecting userId at login
-    #Actually... users have many collections and have many collectionid's... so this wont work.  might need a complex query
-    #This would give a list of collectionid's when we really want a specific collectionid... might need JOIN collection and createcollection
-    #then SELECT collectionid where collection.name = nameOfCollectiontoDelete AND userId = createcollection.userid?
-    collectionId = execute_sql('SELECT collectionid FROM createcollection WHERE userid={}').format(userId)
+def change_name_of_collection(user_id):
+    # Users can modify the name of a collection. They can also delete an entire collection Requirement
 
-    #Definitely going to need collectionid and name to
-    sqlStatement = 'UPDATE collection SET name={} WHERE collectionid={}'.format()
-    return sqlStatement
+    # Prompting the user to get name of collection they want to change and what they want to change it to
+    name_of_collection_to_change = input('Enter name of collection to be re-named')
+    name_of_new_collection = input('Enter new name of collection')
+
+    # appending % to the beginning of the string to make LIKE clause work
+    name_of_collection_to_change = '%' + name_of_collection_to_change
+
+    # This slightly complex query will get us the exact unique collectionId we want in order to find the exact
+    # collection name that needs to be changed
+    collection_id = execute_sql('SELECT collection.collectionid FROM collection INNER JOIN createcollection '
+                                'ON collection.collectionid = createcollection.collectionid '
+                                'WHERE name LIKE {} AND userid={}').format(name_of_collection_to_change, user_id)
+
+    # Finally updating/changing the name of the old collection to the new one
+    sql_statement = 'UPDATE collection SET name={} WHERE collectionid={}'.format(name_of_new_collection, collection_id)
+    return sql_statement
+
 
 main()
