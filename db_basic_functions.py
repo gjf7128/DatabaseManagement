a402294250b534a826a1cecc14ccc5b70a507c6e
gjf7128 <gjf7128@rit.edu>
2023-11-03 20:28:01 -0400

    Added notes on how i'm going to tackle changeNameOfCollection

--- db_basic_functions.py
+++ db_basic_functions.py
@@ -37,15 +37,31 @@ def main():
 def addBookToCollection(collectionid, bookid):
     #Users can add and delete books from their collection Requirement
     #This function is just for ADDING
-    #Use this function like this: cur.execute(addBookToCollection(collectionid, bookid))
+    #This function still needs work
     sqlStatement = 'INSERT INTO contains(collectionid, bookid) VALUES ({}, {})'.format(collectionid, bookid)
-    return sqlStatement
+    return execute_sql(sqlStatement)
 
 def deleteBookFromCollection(collectionid, bookid):
     #Users can add and delete books from their collection Requirement
     #This function is just for DELETING
+    #This function still needs work
+    sqlStatement = 'DELETE FROM contains WHERE collectionid={} AND bookid={}'.format(collectionid, bookid)
+    return execute_sql(sqlStatement)
+
+def changeNameOfCollection(userId):
+    #Users can modify the name of a collection. They can also delete an entire collection Requirement
     #Use this function like this: cur.execute(deleteBookFromCollection(collectionid, bookid))
-    sqlStatement = 'DELETE FROM contains WHERE collectionid= {} AND bookid= {}'.format(collectionid, bookid)
+    nameOfCollectionToDelete = input('Enter name of collection to be re-named')
+    nameOfNewCollection = input('Enter new name of collection')
+
+    #The following LOC depends on collecting userId at login
+    #Actually... users have many collections and have many collectionid's... so this wont work.  might need a complex query
+    #This would give a list of collectionid's when we really want a specific collectionid... might need JOIN collection and createcollection
+    #then SELECT collectionid where collection.name = nameOfCollectiontoDelete AND userId = createcollection.userid?
+    collectionId = execute_sql('SELECT collectionid FROM createcollection WHERE userid={}').format(userId)
+
+    #Definitely going to need collectionid and name to
+    sqlStatement = 'UPDATE collection SET name={} WHERE collectionid={}'.format()
     return sqlStatement
 
 main()
