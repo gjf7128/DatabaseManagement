ec640da6881d42ecc04e254543d834e9ba35d083
jld3877 <jld3877@g.rit.edu>
2023-11-04 21:12:06 -0400

    added cli file, some functionality built

--- db_basic_functions.py
+++ db_basic_functions.py
@@ -31,50 +31,51 @@ def execute_sql(sql, args={}):
         print("Connection failed\n")
         print(e)
 
-def read_book(book_id, person_id, start_page, end_page, date):
+def read_book(book_id, person_id, start_page, end_page):
     # User can read book by selecting the page to start and the page to end
     # There is no read functionality for the user, user should only be able to mark a book as read
     # Function ADDS a new row to "read" table where the book being read, user reading, ammount of pages read, and the date where this rading took place is recorded
     # Function still needs work
     pagesRead = end_page - start_page
-    sqlStatement = 'INSERT INTO read(bookid, userid, pages, date) VALUES ({}, {}, {})'.format(book_id, person_id, pagesRead, date)
+    sqlStatement = 'INSERT INTO read(bookid, userid, pages) VALUES ({}, {}, {})'.format(book_id, person_id, pagesRead)
+    execute_sql(sqlStatement)
 
 def search_books_by_name(title):
     sql_query = "SELECT * FROM book WHERE title LIKE '%" + title + "%';"
     return execute_sql(sql_query)
 
 def search_books_by_release_date_before(date):
-    sql_query = "SELECT * FROM book WHERE releasedate > " + date + ";"
+    sql_query = "SELECT * FROM book WHERE releasedate < CAST('" + date + "' as date);"
     return execute_sql(sql_query)
 
 def search_books_by_release_date_after(date):
-    sql_query = "SELECT * FROM book WHERE releasedate < " + date + ";"
+    sql_query = "SELECT * FROM book WHERE releasedate > CAST('" + date + "' as date);"
     return execute_sql(sql_query)
 
 def search_books_by_author(author):
     sql_query = """SELECT * FROM person LEFT JOIN authors ON person.personID = authors.personID 
-                    LEFT JOIN book ON authors.bookID = book.bookID WHERE person.firstname = """ + author + ";"
+                    LEFT JOIN book ON authors.bookID = book.bookID WHERE person.firstname = '""" + author + "';"
     return execute_sql(sql_query)
 
 def search_books_by_editor(editor):
-    sql_query = """SELECT * FROM person LEFT JOIN edits ON person.personID = editors.personID 
-                    LEFT JOIN book ON editors.bookID = book.bookID WHERE person.firstname = """ + editor + ";"
+    sql_query = """SELECT * FROM person LEFT JOIN edits ON person.personID = edits.personID 
+                    LEFT JOIN book ON edits.bookID = book.bookID WHERE person.firstname = '""" + editor + "';"
     return execute_sql(sql_query)
 
 def search_books_by_publisher(publisher):
     sql_query = """SELECT * FROM person LEFT JOIN publishes ON person.personID = publishes.personID 
-                    LEFT JOIN book ON publishes.bookID = book.bookID WHERE person.firstname = """ + publisher + ";"
+                    LEFT JOIN book ON publishes.bookID = book.bookID WHERE person.firstname = '""" + publisher + "';"
     return execute_sql(sql_query)
 
 def search_books_by_genre(genre):
     sql_query = """SELECT * FROM genre LEFT JOIN bookgenre ON genre.genreID = bookgenre.genreID
-                    LEFT JOIN book ON book.bookID = bookgenre.bookID where genre.name = """ + genre + ";"
+                    LEFT JOIN book ON book.bookID = bookgenre.bookID where genre.name = '""" + genre + "';"
     return execute_sql(sql_query)
 
 def create_collection(userid, collection_name):
-    sql_query = """INSERT INTO collection(name) VALUES(""" + collection_name + ");"
+    sql_query = """INSERT INTO collection(name) VALUES('""" + collection_name + "');"
     execute_sql(sql_query)
-    sql_query = """SELECT collectionid FROM collection where collection_name = """ + collection_name + ";"
+    sql_query = """SELECT collectionid FROM collection where collection_name = '""" + collection_name + "';"
     collectionid = execute_sql(sql_query)
     sql_query = """INSERT INTO createcollection(userid, collection id) VALUES (""" + userid + ',' + collectionid + ");"
 
@@ -88,6 +89,6 @@ def get_collections(userid):
     return execute_sql(sql_query)
 
 def main():
-    print(search_books_by_author(""))
+    print(get_collections(3))
 
 main()
